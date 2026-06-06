e525ec733172d4aa47a8669bd47da5e2aff7d91e
phantomxvoid <phantomxvoid@gmail.com>
2026-06-07 02:49:59 +0530

    third

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/json-toolkit/',   // ← add this line
   plugins: [react(), tailwindcss()],
 })
